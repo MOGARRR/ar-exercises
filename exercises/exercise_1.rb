f134426b5b5d084a4ad7ad20c6d9faf9ad0f07e8
AnthonyA <alicea.9a@gmail.com>
2025-04-10 19:02:10 -0600

    feat: Add three stores to database and confirm they are their with count method

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,19 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+burnaby = Store.new(name:'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+richmond  = Store.new(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.new(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+burnaby.inspect
+richmond.inspect
+gastown.inspect
+
+
+burnaby.save
+richmond.save
+gastown.save
+
+# - Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+# - Richmond (annual_revenue of 1260000 carries women's apparel only)
+# - Gastown (annual_revenue of 190000 carries men's apparel only)

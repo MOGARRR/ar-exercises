cf8299f0ae7d7b11bf7a383c773521811f0eab3a
AnthonyA <alicea.9a@gmail.com>
2025-04-10 19:58:30 -0600

    feat/refactor: Update Store and Employee class to take a one to many relationship and seed employees table with mock data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jenna", last_name: "Larson", hourly_rate: 50)
+@store1.employees.create(first_name: "Leo", last_name: "Nguyen", hourly_rate: 55)
+
+@store2.employees.create(first_name: "Ava", last_name: "Taylor", hourly_rate: 60)
+@store2.employees.create(first_name: "Miles", last_name: "Chan", hourly_rate: 65)
+@store2.employees.create(first_name: "Priya", last_name: "Desai", hourly_rate: 50)
